282ee01a5281c63589d241b53bd7c9be9b6c7529
Suluh Arwani <suluharwani007@gmail.com>
2019-06-21 04:35:49 +0700

    test

---
+++ application/controllers/Login.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Login extends CI_Controller {
+  public function index()
+  {
+
+  }
+}
+/* End of file ${TM_FILENAME:} */
+/* Location: ./${TM_FILEPATH/.+((?:application).+)//:application/controllers/} */
