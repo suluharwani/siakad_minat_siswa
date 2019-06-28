eae0e38fc71fd175153e2270cae05f43f3b2b87d
Suluh Arwani <suluharwani007@gmail.com>
2019-06-28 23:28:20 +0700

    login view

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,6 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Login extends CI_Controller {
   public function index()
   {
+    $data['title'] = "Login";
+    $this->load->view('login/login', $data);
 
   }
 }
